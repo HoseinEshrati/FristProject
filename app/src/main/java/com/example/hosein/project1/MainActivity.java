8cf3f18661988c3eb45d5e99090d763195c58d24
HOSEIN\Hosein <eshrati@outlook.com>
2016-12-10 14:04:35 +0330

    start project and creat view dialog

--- app/src/main/java/com/example/hosein/project1/MainActivity.java
+++ app/src/main/java/com/example/hosein/project1/MainActivity.java
@@ -1,13 +1,38 @@
 package com.example.hosein.project1;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.btnad);
+button.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+
+
+AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+
+
+
+    }
+});
+
+
+
+
+
+
     }
 }
